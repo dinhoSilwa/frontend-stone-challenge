3598837cbd84d9826991d20799ac20a465cccda8
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-07 15:15:00 -0300

    style : modify style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 
 import './App.css'
 import { Form } from './components/FinancialForm'
+import bg from './assets/Mask.png'
 
 function App() {
 
   return (
-<main>
-  <Form />
+<main className='w-full h-screen overflow-hidden relative'>
+<img src={bg} alt="background-stone" className='top-[-100px] left-0 w-full absolute border-red-2 -z-50' />
+<header>Stone aqui</header>
+ <Form  />
 </main>
   )
 }
