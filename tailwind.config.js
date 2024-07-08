e48997a0c82bc8cc8b657e4a306b531f63cfecb1
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-08 07:44:36 -0300

    STYLE : add color vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ export default {
       fontFamily: {
         'sharon': ['Sharon - Personal Use Regular', 'sans-serif'],
       },
+      colors : {
+        "primary-text" : '#45505E',
+        'second-text' : '#8C9CAD',
+        'stone-color' : "#00AB63"
+      }
     },
   },
   plugins: [],
