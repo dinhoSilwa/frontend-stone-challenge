70da546b754eb7ccf2f7b84427c06eec3808af5e
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-08 09:51:57 -0300

    STYLE: personal variables of sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,14 @@ export default {
         "primary-text" : '#45505E',
         'second-text' : '#8C9CAD',
         'stone-color' : "#00AB63"
-      }
+      },
+      screens: {
+        'mob-small': '640px',    
+        'mob-medium': '768px',   
+        'desk-small': '1024px',   
+        'desk-medium': '1280px',
+        'desk-larger': '1536px',  
+      },
     },
   },
   plugins: [],
